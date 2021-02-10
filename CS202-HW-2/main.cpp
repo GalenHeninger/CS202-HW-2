e794b17227c1a8f38e9bf00a0c571d6824196f54
Galen Heninger <heninger.galen@gmail.com>
2021-02-10 11:29:13 -0900

    Initial Commit

---
+++ CS202-HW-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS202-HW-2
+//
+//  Created by Galen on 2/10/21.
+//  Copyright © 2021 Galen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
