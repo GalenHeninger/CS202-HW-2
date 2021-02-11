c7cf9d0f5da3f2af4b7b3a19e921c6d9a38d9949
Galen Heninger <heninger.galen@gmail.com>
2021-02-11 14:09:23 -0900

    std::list with Value struct

--- Program 1/main.cpp
+++ Program 1/main.cpp
@@ -7,16 +7,31 @@
 //
 
 #include <iostream>
+#include <list>
+using std::list;
 
 struct Value {
-    
     int x;
-    Value * p;
-    
+//    Value * p;
 };
 
 int main() {
     
+//    //v0 points to the first value in the list
+//    Value * root;
+//    root = new Value;
+//    //End at the root of the list with null pointer
+//    //root->p = nullptr;
+//    Value * pointer_to_element;
+//    // Start at root
+//    pointer_to_element = root;
+//    // Push a value to the end of the list
+//    pointer_to_element->p = new Value;
+    
+    //Linked list with list header
+    list<Value> a_list;
+    Value A;
+    a_list.push_back(A);
     
     
     return 0;
