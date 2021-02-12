8467cb9e1ae8e0d5282044e0b2640d67d0d16968
Galen Heninger <heninger.galen@gmail.com>
2021-02-11 16:21:46 -0900

    testing with Gutenberg library metadata

--- Program 1/main.cpp
+++ Program 1/main.cpp
@@ -11,22 +11,49 @@
 #include <list>
 #include "catch.h"
 using std::list;
+using std::string;
 
-struct Value {
-    int x;
+struct metadata {
+    string author;
+    string url;
+    string language;
+    string rights;
+    string subject;
+    string title;
 };
 
+// Testing std::list
 TEST_CASE() {
     
-    Value A;
-    Value B;
+    metadata A;
+    A.author = "Jefferson, Thomas";
+    A.url = "http://www.gutenberg.org/files/1/1-0.txt";
+    A.language = "en";
+    A.rights = "Public domain in the USA.";
+    A.subject = "United States -- History -- Revolution, 1775-1783 -- Sources";
+    A.title = "The Declaration of Independence of the United States of America";
     
-    //Linked list with list header
-    list<Value> list1({A});
-    list<Value> list2({A, B});
+    
+    metadata B;
+    A.author = "Kennedy, John F. (John Fitzgerald)";
+    A.url = "http://www.gutenberg.org/files/3/3.txt";
+    A.language = "en";
+    A.rights = "Public domain in the USA.";
+    A.subject = "United States -- Foreign relations -- 1961-1963";
+    A.title = "John F. Kennedy's Inaugural Address";
+    
+    
+    // Linked list with list header
+    list<metadata> list1({A});
+    list<metadata> list2({A, B});
     
     list1.push_back(B);
     
+    // Testing the equality of the actual lists causes an error
     REQUIRE(list1.size() == list2.size());
+    
+    list1.pop_front();
+    REQUIRE(list1.size() == 1);
+    
     
 };
