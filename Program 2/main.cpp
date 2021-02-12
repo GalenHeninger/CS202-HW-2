88b84b6bcf2bc5dc546a3d17bb7c955720b01762
Galen Heninger <heninger.galen@gmail.com>
2021-02-12 14:34:09 -0900

    Comparison of the addresses of multiple variables

--- Program 2/main.cpp
+++ Program 2/main.cpp
@@ -8,13 +8,14 @@
 
 #include <iostream>
 using std::cout;
+using std::endl;
 
-void printPointer(int i) {
- int* iptr = &i;
- // the unary * operator dereferences the pointer
- //std::cout << "i = " << *iptr << "\n";
- std::cout << "&i = " << iptr << "\n";
-}
+//void printPointer(int i) {
+// int* iptr = &i;
+// // the unary * operator dereferences the pointer
+// //std::cout << "i = " << *iptr << "\n";
+// std::cout << "&i = " << iptr << "\n";
+//}
 
 int main() {
     // A storage class in the context of C++ variable declarations is a type specifier that governs the lifetime, linkage, and memory location of objects. (The keyword static alters the memory location.)
@@ -22,15 +23,44 @@ int main() {
     // Stack: The allocation happens on contiguous blocks of memory. Whenever a function is called, its variables get memory allocated on the stack
     // Free store: The region of memory used for dynamic memory management
     
-    static int i = 0; // Static storage
-    int j = 1; // Memory allocated on stack
-    int *p = new int(2); // Memory allocated dynamically "Free store"
+    cout << "Addresses of integer variables:\n";
+    cout << endl;
     
-    cout << &i << " (Static storage)\n";
-    cout << &j << " (Allocated on stack)\n";
-    cout << p << " (Allocated dynamically)\n";
-    //std::cout << "i = " << *p << "\n";
-    //std::cout << "&i = " << p << "\n";
+    // Static storage
+    static int i = 0;
+    static int j = 2;
+    static int k = 3;
+    
+    cout << "Static storage:\n";
+    cout << &i << "\n";
+    cout << &j << "\n";
+    cout << &k << "\n";
+    cout << endl;
+    
+    
+    // Memory allocated on stack
+    int i1 = 0;
+    int j1 = 1;
+    int k1 = 2;
+    
+    cout << "Allocated on stack:\n";
+    cout << &i1 << "\n";
+    cout << &j1 << "\n";
+    cout << &k1 << "\n";
+    cout << endl;
+    cout << "The addresses of the variables created later are lower addresses.\n";
+    cout << endl;
+    
+    // Memory allocated dynamically
+    //"Free store"
+    int *p1 = new int(0);
+    int *p2 = new int(1);
+    int *p3 = new int(2);
+    cout << "Allocated dynamically:\n";
+    cout << p1 << "\n";
+    cout << p2 << "\n";
+    cout << p3 << "\n";
+    cout << endl;
     
     return 0;
 }
