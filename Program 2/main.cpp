1a5eafcfaa8e6c0ecfc7a2f1200f5b1887991d57
Galen Heninger <heninger.galen@gmail.com>
2021-02-12 15:44:54 -0900

    Now using array for dynamically allocated var

--- Program 2/main.cpp
+++ Program 2/main.cpp
@@ -10,21 +10,16 @@
 using std::cout;
 using std::endl;
 
-//void printPointer(int i) {
-// int* iptr = &i;
-// // the unary * operator dereferences the pointer
-// //std::cout << "i = " << *iptr << "\n";
-// std::cout << "&i = " << iptr << "\n";
-//}
 
 int main() {
     // A storage class in the context of C++ variable declarations is a type specifier that governs the lifetime, linkage, and memory location of objects. (The keyword static alters the memory location.)
-    // Static storage: Static variables preserve their value even after they are out of their scope!
+    // Static storage: Static variables preserve their value even after they are out of their scope
     // Stack: The allocation happens on contiguous blocks of memory. Whenever a function is called, its variables get memory allocated on the stack
     // Free store: The region of memory used for dynamic memory management
     
     cout << "Addresses of integer variables:\n";
     cout << endl;
+    
     
     // Static storage
     static int i = 0;
@@ -48,18 +43,25 @@ int main() {
     cout << &j1 << "\n";
     cout << &k1 << "\n";
     cout << endl;
-    cout << "The addresses of the variables created later are lower addresses.\n";
+    cout << "On the stack the addresses of the variables created later are lower addresses.\n";
     cout << endl;
+    
     
     // Memory allocated dynamically
     //"Free store"
-    int *p1 = new int(0);
-    int *p2 = new int(1);
-    int *p3 = new int(2);
-    cout << "Allocated dynamically:\n";
-    cout << p1 << "\n";
-    cout << p2 << "\n";
-    cout << p3 << "\n";
+    int * p = new int[3];
+    
+    p[0] = 0;
+    p[1] = 1;
+    p[2] = 2;
+    
+    cout << "Dynamically allocated array:\n";
+    cout << &p[0] << "\n";
+    cout << &p[1] << "\n";
+    cout << &p[2] << "\n";
+    cout << endl;
+    
+    cout << "The addresses of the elements of the dynamically allocated array increase for higher indices.\n";
     cout << endl;
     
     return 0;
